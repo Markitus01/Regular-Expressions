af171bfdcc559cb1b84611938e11e7cc75fde89f
Μαρκος01 <marksand2001@gmail.com>
2023-11-30 19:03:22 +0100

    Update RegExp3.js

--- RegExp3.js
+++ RegExp3.js
@@ -4,9 +4,9 @@ function ex2()
 {
     let caixa_msg = document.getElementById("aa");
     let caixa_text = document.getElementById("c");
-    caixa_text.defaultValue = "Canvi d'inventari";
+    caixa_text.value = "Canvi d'iNVentari";
 
-    let er_match = /nv/gm; //global i multiline, per a que no faci el return amb el primer match ni al acabar la linea
+    let er_match = /NV/g; //global, per a que no faci el return amb el primer match 
 
     document.getElementById("b2").addEventListener("click", (evt) =>
     {
@@ -16,18 +16,20 @@ function ex2()
 
         if (array != null && array.length <= 3) // Si coincideix 3 vegades o menys, mostrem per consola
         {
-            console.info("S'ha trobat el patró nv: "+array.length+" vegades");
+            console.info("S'ha trobat el patró NV: "+array.length+" vegades");
         }
         else if (array != null && array.length > 3) // Si coincideix +3 vegades mostrem al span
         {
             caixa_msg.style.color = "green";
-            caixa_msg.textContent = "S'ha trobat el patró nv: "+array.length+" vegades";
+            caixa_msg.textContent = "S'ha trobat el patró NV: "+array.length+" vegades";
         }
         else // ERROR al span si no hi ha cap
         {
             caixa_msg.style.color = "red";
             caixa_msg.textContent = "ERROR";
         }
+
+        caixa_text.value = "Canvi d'iNVentari";
         
         evt.preventDefault();
     });
